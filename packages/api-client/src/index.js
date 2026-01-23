fb045242c7c0f145bb761ea07a7fad3e2e8911b7
Bret Mda <brtdev2@gmail.com>
2026-01-23 14:59:10 +0300

    Test deployment 2

--- packages/api-client/src/index.js
+++
@@ -1,7 +0,0 @@
-import { hc } from "hono/client";
-
-export const createClient = (baseUrl = "/") => hc(baseUrl);
-export const client = createClient(typeof window === "undefined"
-  // eslint-disable-next-line node/no-process-env
-  ? process.env.API_URL || "http://localhost:4000/"
-  : "/");
--- packages/api-client/src/index.ts
+++ packages/api-client/src/index.ts
@@ -5,8 +5,8 @@ import { hc } from "hono/client";
 
 export function createClient() {
   // In browser, use relative path; in SSR, use full URL
-  const baseUrl
-    = typeof window !== "undefined"
+  const baseUrl =
+    typeof window !== "undefined"
       ? process.env.NEXT_PUBLIC_API_URL || "/api"
       : process.env.API_URL || "http://localhost:4000";
 
