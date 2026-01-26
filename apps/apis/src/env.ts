02853eb61ca48ce8107c07eaf0120efea3e60ea4
Bret Mda <brtdev2@gmail.com>
2026-01-26 10:08:45 +0300

    Update apis env config

--- apps/apis/src/env.ts
+++ apps/apis/src/env.ts
@@ -16,7 +16,6 @@ expand(
 const EnvSchema = z
   .object({
     NODE_ENV: z.string().default("development"),
-    NODEJS_HELPERS: z.number().optional(),
     PORT: z.coerce.number().default(9999),
     LOG_LEVEL: z.enum([
       "fatal",
@@ -44,14 +43,6 @@ const EnvSchema = z
         expected: "string",
         received: "undefined",
         path: ["DATABASE_AUTH_TOKEN"],
-        message: "Must be set when NODE_ENV is 'production'",
-      });
-    } else if (input.NODE_ENV === "production" && !input.NODEJS_HELPERS) {
-      ctx.addIssue({
-        code: z.ZodIssueCode.invalid_type,
-        expected: "number",
-        received: "undefined",
-        path: ["NODEJS_HELPERS"],
         message: "Must be set when NODE_ENV is 'production'",
       });
     }
