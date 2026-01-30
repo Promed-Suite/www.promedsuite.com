df90f3eeac8e244c4a53a0876962f976b7e2f931
Bret Mda <brtdev2@gmail.com>
2026-01-30 15:30:28 +0300

    Setup linting configs for VS editor

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+// This configuration only applies to the package manager root.
+import config from "./packages/eslint-config/base.js";
+
+export default config;
+
+// import createConfig from "./packages/eslint-config/create-config.js";
+
+// export default createConfig({
+//   ignores: ["apps/*", "packages/*", "drizzle/*"],
+// });
